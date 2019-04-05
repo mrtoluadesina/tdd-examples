b941e54a85b6ff00025a9a6af330948b1b37b980
Tolu Adesina <tadesina90@gmail.com>
2019-04-05 18:27:03 +0100

    Test Case - Dividing a positive number by a negative number

--- division/divide.test.js
+++ division/divide.test.js
@@ -3,3 +3,7 @@ const divide = require('./divide');
 test('Divide the first parameter of a function by the second, - both are integers', () => {
   expect(divide(15, 3)).toBe(5);
 });
+
+test('divide a positive number by a negative number', () => {
+  expect(divide(10, -2)).toBe(-5);
+});
