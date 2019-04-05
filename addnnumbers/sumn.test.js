6b4c2574a4c0892f5b1283458a19f943207defec
Tolu Adesina <tadesina90@gmail.com>
2019-04-05 13:18:36 +0100

    Another test written for adding strings to equal NaN

--- addnnumbers/sumn.test.js
+++ addnnumbers/sumn.test.js
@@ -12,6 +12,10 @@ test('add numbers and string', () => {
   expect(sumn(2, 5, 'decagon')).toBe('NaN')
 });
 
+test('adding strings to equal NaN', () => {
+  expect(sumn('decagon', 'institute')).toBe('NaN');
+});
+
 test('adding decimal numbers', () => {
   expect(sumn(0.1, 0.2)).toBeCloseTo(0.3);
 });
