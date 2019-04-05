24ce31b7051e16e43d2ccc0bf1b55e6714b73548
Tolu Adesina <tadesina90@gmail.com>
2019-04-05 17:43:45 +0100

    Function to solve the multiplication of strings added to former multiply function

--- multiply/multiply.js
+++ multiply/multiply.js
@@ -1,7 +1,11 @@
 function multiply() {
   var times = 1;
   for (var i = 0; i < arguments.length; i++) {
-    times *= arguments[i];
+    if (isNaN(arguments[i])) {
+      return 'NaN';
+    } else {
+      times *= arguments[i];
+    }
   }
   return times;
 }
