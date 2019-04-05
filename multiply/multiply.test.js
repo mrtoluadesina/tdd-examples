ffb13bdf353abd75084bc8a1f7d4f237394797be
Tolu Adesina <tadesina90@gmail.com>
2019-04-05 17:36:37 +0100

    Multiplying 5 numbers, edge case test created and passes through function

--- multiply/multiply.test.js
+++ multiply/multiply.test.js
@@ -3,3 +3,7 @@ const multiply = require('./multiply');
 test('Multiply 2 numbers', () => {
   expect(multiply(2, 4)).toBe(8);
 });
+
+test('Multiply 5 numbers', () => {
+  expect(multiply(3, 5, 2, 7, 6)).toBe(1260);
+});
