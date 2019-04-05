80f947aa6d8beaaabf4df374d23362ee27e32c34
Tolu Adesina <tadesina90@gmail.com>
2019-04-05 16:23:15 +0100

    Test added to minus test file to cater for the values that are not numbers

--- difference/minus.test.js
+++ difference/minus.test.js
@@ -3,3 +3,7 @@ const minus = require('./minus');
 test('Substract a number from the other, 4 - 2 should equal to 2', () => {
   expect(minus(4, 2)).toBe(2);
 });
+
+test('subtracting one string and a number, 10 - david', () => {
+  expect(minus(10, 'david')).toBe('NaN');
+});
