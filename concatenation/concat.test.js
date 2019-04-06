37ec1a99c893435bf0881353cc8a2b372f928fdd
Tolu Adesina <tadesina90@gmail.com>
2019-04-06 16:12:08 +0100

    Test Case - Test created for concatenating a string and a number represented as a string together

--- concatenation/concat.test.js
+++ concatenation/concat.test.js
@@ -3,3 +3,7 @@ const concat = require('./concat');
 test('Concatenating to strings', () => {
   expect(concat('Decagon', ' Institute')).toBe('Decagon Institute');
 });
+
+test('Concatenating a number in string format with a string', () => {
+  expect(concat('L', '5')).toBe('L5');
+});
