562dd16ea1d13bceb651cd4dd4938ca62f4fc3da
Tolu Adesina <tadesina90@gmail.com>
2019-04-08 13:22:30 +0100

    Test Case - Updated tests with a test to check when function is called with invalid argument or none at all

--- githubapicall/apicall.spec.js
+++ githubapicall/apicall.spec.js
@@ -2,8 +2,17 @@ const { login } = require('./apicall');
 
 jest.mock('./apicall');
 
-test('username is available on github', () => {
+test('Function is defined', () => {
   return login("mrtoluadesina").then(function(response) {
     expect(response).toBeDefined();
-  })
+  });
+});
+
+test('Function is declared without argument', () => {
+  // return login().then(function(response) {
+  //   expect(response).toThrow('Needs an argument');
+  // });
+  expect(function() {
+    return login();
+  }).toThrow('Needs argument');
 });
